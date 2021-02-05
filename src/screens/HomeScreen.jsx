85d29eaa704ea60e17c8a62812b8daccb14c1794
Yarikks <jasper18poil@gmail.com>
2021-02-05 22:23:07 +0200

    Navigating Users Between Screens

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const HomeScreen = () => <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  const navigate = (route) => {
+    navigation.navigate(route);
+  };
+
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigate('Components')}>
+        <Text style={styles.buttonText}>Go to Components</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigate('List')}>
+        <Text style={styles.buttonText}>Go to List</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    textAlign: 'center',
+  },
+  button: {
+    marginVertical: 20,
+    alignSelf: 'center',
+    padding: 10,
+    backgroundColor: 'black',
+    borderRadius: 12,
+    width: '50%',
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
